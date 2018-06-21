340bf40fceeb6645405a6382da328733eb24f83d
ohyear1987 <ohyear1987@live.com>
2018-06-21 18:46:48 +0800

    活动签到，活动取消和修改等功能

--- wechat/build/webpack.base.conf.js
+++ wechat/build/webpack.base.conf.js
@@ -4,19 +4,19 @@ var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
 
-function resolve (dir) {
-  return path.join(__dirname, '..', dir)
+function resolve(dir) {
+    return path.join(__dirname, '..', dir)
 }
 
-function getEntry (dir, entryFile) {
-  const files = fs.readdirSync(dir)
-  return files.reduce((res, k) => {
-    const page = path.resolve(dir, k, entryFile)
-    if (fs.existsSync(page)) {
-      res[k] = page
-    }
-    return res
-  }, {})
+function getEntry(dir, entryFile) {
+    const files = fs.readdirSync(dir)
+    return files.reduce((res, k) => {
+        const page = path.resolve(dir, k, entryFile)
+        if (fs.existsSync(page)) {
+            res[k] = page
+        }
+        return res
+    }, {})
 }
 
 const appEntry = { app: resolve('./src/main.js') }
@@ -26,76 +26,75 @@ const entry = Object.assign({}, appEntry, pagesEntry)
 process.noDeprecation = true
 
 module.exports = {
-  entry: entry,
-  target: require('mpvue-webpack-target'),
-  output: {
-    path: config.build.assetsRoot,
-    filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
-  },
-  resolve: {
-    extensions: ['.js', '.vue', '.json'],
-    alias: {
-      'vue': 'mpvue',
-      '@': resolve('src')
+    entry: entry,
+    target: require('mpvue-webpack-target'),
+    output: {
+        path: config.build.assetsRoot,
+        filename: '[name].js',
+        publicPath: process.env.NODE_ENV === 'production' ?
+            config.build.assetsPublicPath : config.dev.assetsPublicPath
     },
-    symlinks: false
-  },
-  module: {
-    rules: [
-      // {
-      //   test: /\.(js|vue)$/,
-      //   loader: 'eslint-loader',
-      //   enforce: 'pre',
-      //   include: [resolve('src'), resolve('test')],
-      //   options: {
-      //     formatter: require('eslint-friendly-formatter')
-      //   }
-      // },
-      {
-        test: /\.vue$/,
-        loader: 'mpvue-loader',
-        options: vueLoaderConfig
-      },
-      {
-        test: /\.js$/,
-        include: [resolve('src'), resolve('test')],
-        use: [
-          'babel-loader',
-          {
-            loader: 'mpvue-loader',
-            options: {
-              checkMPEntry: true
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue': 'mpvue',
+            '@': resolve('src')
+        },
+        symlinks: false
+    },
+    module: {
+        rules: [
+            // {
+            //   test: /\.(js|vue)$/,
+            //   loader: 'eslint-loader',
+            //   enforce: 'pre',
+            //   include: [resolve('src'), resolve('test')],
+            //   options: {
+            //     formatter: require('eslint-friendly-formatter')
+            //   }
+            // },
+            {
+                test: /\.vue$/,
+                loader: 'mpvue-loader',
+                options: vueLoaderConfig
+            },
+            {
+                test: /\.js$/,
+                include: [resolve('src'), resolve('test'), /mpvue-wxparse/],
+                use: [
+                    'babel-loader',
+                    {
+                        loader: 'mpvue-loader',
+                        options: {
+                            checkMPEntry: true
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: utils.assetsPath('img/[name].[ext]')
+                }
+            },
+            {
+                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: utils.assetsPath('media/[name]].[ext]')
+                }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: utils.assetsPath('fonts/[name].[ext]')
+                }
             }
-          },
         ]
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[ext]')
-        }
-      },
-      {
-        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('media/[name]].[ext]')
-        }
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[ext]')
-        }
-      }
-    ]
-  }
+    }
 }
---
+++ wechat/src/pages/checkin/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import App from './checkin'
+
+const app = new Vue(App)
+app.$mount()
+
+// console.log(app)
