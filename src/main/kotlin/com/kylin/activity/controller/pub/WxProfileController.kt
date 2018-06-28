8d5ea6ee80f9ccbab51c30408818aab86fdb552b
yangweiji <weiji.yang@9kylin.cn>
2018-06-28 21:12:02 +0800

    完成微信小程序：‘我的活动’后台控制器，调整个人中心4个活动数+积分总额

--- src/main/kotlin/com/kylin/activity/controller/pub/WxProfileController.kt
+++ src/main/kotlin/com/kylin/activity/controller/pub/WxProfileController.kt
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("pub/wx/profile")
 class WxProfileController {
+    /**
+     * 时间转换
+     */
     @Autowired
     private val util: KylinUtil? = null
 
@@ -48,68 +51,17 @@ class WxProfileController {
 
 
     /**
-     * 用户已参加活动数
-     * @param userId 用户id
-     * @param communityId 团体id
+     * 个人中心4个活动数+积分总额
+     * counts:已参加/我喜欢/需签到/已签到/积分总额
+     * @param userId 用户编号
+     * @param communityId 团体编号
      */
     @CrossOrigin
-    @GetMapping("/getActivityAttendCounts")
-    fun activityAttendCounts(@RequestParam(required = false) userId: Int?,
-                             @RequestParam(required = false) communityId: Int?): InfoData {
-        return proFileService!!.activityAttendCounts(userId, communityId)
-    }
-
-
-    /**
-     * 用户收藏活动数
-     * @param userId 用户id
-     * @param communityId 团体id
-     */
-    @CrossOrigin
-    @GetMapping("/getFavoriteActivityCounts")
-    fun favoriteActivityCounts(@RequestParam(required = false) userId: Int?,
-                               @RequestParam(required = false) communityId: Int?): InfoData {
-        return proFileService!!.favoriteActivityCounts(userId, communityId)
-    }
-
-
-    /**
-     * 用户需签到活动数
-     * @param userId 用户id
-     * @param communityId 团体id
-     *
-     */
-    @CrossOrigin
-    @GetMapping("/getNoCheckedActivityCounts")
-    fun noCheckedActivityCounts(@RequestParam(required = false) userId: Int?,
-                                @RequestParam(required = false) communityId: Int?): InfoData {
-        return proFileService!!.noCheckedActivityCounts(userId, communityId)
-    }
-
-
-    /**
-     * 用户已签到活动数
-     * @param userId 用户id
-     * @param communityId 团体id
-     */
-    @CrossOrigin
-    @GetMapping("/getCheckedActivityCounts")
-    fun checkedActivityCounts(@RequestParam(required = false) userId: Int?,
-                              @RequestParam(required = false) communityId: Int?): InfoData {
-        return proFileService!!.checkedActivityCounts(userId, communityId)
-    }
-
-
-    /**
-     * 用户积分总额
-     * @param userId 用户id
-     * @param communityId 团体id
-     */
-    @CrossOrigin
-    @GetMapping("/getSumScores")
-    fun sumScores(@RequestParam(required = false) userId: Int?,
-                  @RequestParam(required = false) communityId: Int?): InfoData {
-        return proFileService!!.sumScores(userId, communityId)
+    @GetMapping("/getPersonalInfoCounts")
+    fun getPersonalInfoCounts(@RequestParam(required = false) userId: Int?,
+                              @RequestParam(required = false) communityId: Int?): List<Any> {
+        val personalInfoCounts = proFileService!!.getPersonalInfoCounts(userId, communityId)
+        return personalInfoCounts.intoMaps()
     }
 
 
@@ -166,18 +118,13 @@ class WxProfileController {
         for (activity in activitiesList) {
             var map = mutableMapOf<String, Any?>()
             var avatar: String? = null
-            var user_avatar: String? = null
             if (activity["avatar"] != null) {
                 avatar = commonService!!.getDownloadUrl(activity.get("avatar", String::class.java), "middle")
-            }
-            if (activity["user_avatar"] != null) {
-                user_avatar = commonService!!.getDownloadUrl(activity.get("user_avatar", String::class.java), "middle")
             }
             map["id"] = activity.get("id", Int::class.java)
             map["community_id"] = activity.get("community_id", Int::class.java)
             map["title"] = activity.get("title").toString()
             map["avatar"] = avatar
-            map["user_avatar"] = user_avatar
             map["start_time"] = util!!.fromNow(activity.get("start_time"))
             map["end_time"] = util!!.fromNow(activity.get("end_time"))
             map["attend_count"] = activity.get("attend_count", Int::class.java)
@@ -186,6 +133,4 @@ class WxProfileController {
         }
         return items
     }
-
-
 }
