b2b9a6531106b868613cced8f168acba851698bd
yangweiji <weiji.yang@9kylin.cn>
2018-07-02 10:31:16 +0800

    调整PC首页活动信息：点击活动进入活动详情页面，有活动，显示活动相册，没有，则隐藏

--- src/main/java/com/kylin/activity/databases/tables/records/CommunityRecord.java
+++ src/main/java/com/kylin/activity/databases/tables/records/CommunityRecord.java
@@ -5,14 +5,16 @@ package com.kylin.activity.databases.tables.records;
 
 
 import com.kylin.activity.databases.tables.Community;
+
+import java.sql.Timestamp;
+
+import javax.annotation.Generated;
+
 import org.jooq.Field;
 import org.jooq.Record1;
 import org.jooq.Record20;
 import org.jooq.Row20;
 import org.jooq.impl.UpdatableRecordImpl;
-
-import javax.annotation.Generated;
-import java.sql.Timestamp;
 
 
 /**
