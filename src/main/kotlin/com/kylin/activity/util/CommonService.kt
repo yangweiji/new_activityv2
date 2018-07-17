8a2de4e2b9061569eacd097b8d4d3c9b5df21b3d
cf_hc1980 <cf_hc1980@aliyun.com>
2018-07-17 22:15:15 +0800

    1、身份短信配置调整，统一从SmsTemplateList中读取配置信息 2、快捷登录、忘记密码，请求短信发送，按照templateId=3来读取 3、配置文件调整处理

--- src/main/kotlin/com/kylin/activity/util/CommonService.kt
+++ src/main/kotlin/com/kylin/activity/util/CommonService.kt
@@ -9,9 +9,10 @@ import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest
 import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse
 import com.aliyuncs.exceptions.ClientException
 import com.aliyuncs.profile.DefaultProfile
-import org.springframework.stereotype.Service
+import com.kylin.activity.sms.SmsTemplateListProperties
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
+import org.springframework.stereotype.Service
 import java.util.*
 
 
@@ -39,12 +40,30 @@ class CommonService {
     val activitySmsSign: String get() = env!!.getProperty("sms.sign")
 
     /**
+     * 短信配置
+     */
+    @Autowired
+    val smsTemplateListProperties: SmsTemplateListProperties? = null
+
+    /**
      * 发送模板消息短息：接收短信码
      * @param mobile: 手机号
      * @param code: 短信验证
      */
     @Throws(ClientException::class)
-    fun sendSms(mobile:String, code:String): SendSmsResponse {
+    fun sendSms(mobile: String, code: String): SendSmsResponse {
+        //按照用户注册短信模板来发送短信
+        return sendSms(mobile, code, 2)
+    }
+
+    /**
+     * 发送模板消息短息：接收短信码
+     * @param mobile: 手机号
+     * @param code: 短信验证
+     * @param templateId: 模板ID
+     */
+    @Throws(ClientException::class)
+    fun sendSms(mobile: String, code: String, templateId: Int): SendSmsResponse {
 
         //可自助调整超时时间
         System.setProperty("sun.net.client.defaultConnectTimeout", "10000")
@@ -58,14 +77,13 @@ class CommonService {
         //组装请求对象-具体描述见控制台-文档部分内容
         val request = SendSmsRequest()
         //必填:待发送手机号
-        request.setPhoneNumbers(mobile)
+        request.phoneNumbers = mobile
         //必填:短信签名-可在短信控制台中找到
-        request.setSignName(smsSign)
+        request.signName = smsTemplateListProperties!!.sign
         //必填:短信模板-可在短信控制台中找到
-        request.setTemplateCode(smsTemplate)
+        request.templateCode = smsTemplateListProperties!!.templates!![templateId].code
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
-        request.setTemplateParam("{\"code\":\"" + code + "\"}")
-
+        request.templateParam = "{\"code\":\"$code\"}"
         //选填-上行短信扩展码(无特殊需求用户请忽略此字段)
         //request.setSmsUpExtendCode("90997");
 
@@ -126,12 +144,12 @@ class CommonService {
         val postSignature = client.calculatePostSignature(postPolicy)
 
         val respMap = LinkedHashMap<String, String>()
-        respMap.put("accessid", accessKeyId)
-        respMap.put("policy", encodedPolicy)
-        respMap.put("signature", postSignature)
-        respMap.put("dir", dir)
-        respMap.put("host", host)
-        respMap.put("expire", (expireEndTime / 1000).toString())
+        respMap["accessid"] = accessKeyId
+        respMap["policy"] = encodedPolicy
+        respMap["signature"] = postSignature
+        respMap["dir"] = dir
+        respMap["host"] = host
+        respMap["expire"] = (expireEndTime / 1000).toString()
         return respMap
     }
 
@@ -141,13 +159,13 @@ class CommonService {
      * @param style: 样式
      * @param subDir: 子目录
      */
-    fun getDownloadUrl(fileId:String, style:String? = null, subDir:String? = null):String {
-        if(fileId.startsWith("http://", true) || fileId.startsWith("https://", true)){
+    fun getDownloadUrl(fileId: String, style: String? = null, subDir: String? = null): String {
+        if (fileId.startsWith("http://", true) || fileId.startsWith("https://", true)) {
             return fileId
         }
-        var subDir = if(subDir == null) dir else subDir
+        var subDir = if (subDir == null) dir else subDir
         var url = "https://${bucket}.${endpoint}/${subDir}/${fileId}"
-        if(style != null && !style.isNullOrBlank()){
+        if (style != null && !style.isNullOrBlank()) {
             url += "?x-oss-process=style/" + style
         }
         return url
