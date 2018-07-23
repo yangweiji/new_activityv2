b04ef67730c9904a5b69d368e81dbf4815aeda4c
阿凯160508 <904621245@qq.com>
2018-07-23 13:47:17 +0800

    微信搜索栏 时间格式及喜欢人数修改

--- src/main/kotlin/com/kylin/activity/controller/pub/WxSearchController.kt
+++ src/main/kotlin/com/kylin/activity/controller/pub/WxSearchController.kt
@@ -1,17 +1,28 @@
 package com.kylin.activity.controller.pub
 
 import com.kylin.activity.service.ActivityService
+import com.kylin.activity.util.CommonService
+import com.kylin.activity.util.KylinUtil
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import java.text.ParseException
 
-
 /**
- * Created by 9kylin on 2018-06-12.
+ * Created by zk on 2018-07-21.
  */
 @RestController
 @RequestMapping("pub/wx/search")
 class WxSearchController  {
+    /**
+     * 第三方工具
+     */
+    @Autowired
+    private val util: KylinUtil? = null
+    /**
+     * 通用服务
+     */
+    @Autowired
+    private var commonService: CommonService? = null
     /**
      * 活动服务
      */
@@ -35,9 +46,27 @@ class WxSearchController  {
           ): Any {
         //取得活动信息
         var activities= activityService!!.getPublicActivities(tag, time, "",searchText)
-        var result = mutableListOf<Any>()
-        if(activities.isNotEmpty)
-        activities.forEach {  result.add(it.intoMap())}
+        var result = mutableListOf<MutableMap<String, Any?>>()
+
+        for (activity in activities) {
+            var map = mutableMapOf<String, Any?>()
+            var avatar: String? = null
+            if (activity["avatar"] != null) {
+                avatar = commonService!!.getDownloadUrl(activity.get("avatar", String::class.java), "middle")
+            }
+            map["id"] = activity.get("id", Int::class.java)
+            map["activity_type"] = activity.get("activity_type", Int::class.java)
+            map["favorite_count"] = activity.get("favorite_count", Int::class.java)
+            map["attend_count"] = activity.get("attend_user_count", Int::class.java)
+            map["avatar"] = avatar
+            map["start_time"] = util!!.fromNow(activity.get("start_time"))
+            map["title"] = activity.get("title").toString()
+            result.add(map)
+        }
+
+        //if(activities.isNotEmpty)
+        //activities.forEach {  result.add(it.intoMap())}
+
         return result
     }
 }
