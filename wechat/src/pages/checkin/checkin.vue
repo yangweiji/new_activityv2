e7e1a2f5ff1cecc87747235c4c267fa9db13bea9
阿凯160508 <904621245@qq.com>
2018-07-24 11:10:29 +0800

    中签提示语异常

--- wechat/src/pages/checkin/checkin.vue
+++ wechat/src/pages/checkin/checkin.vue
@@ -9,11 +9,14 @@
         <h1 class="c-title-text" v-else-if="!item.checkInUserId">
           您未报名
         </h1>
-        <h1 class="c-title-text" v-else-if="item.activity.activity_type == 3 && item.status != 2">
+        <h1 class="c-title-text" v-else-if="item.activity.activity_type == 3 && item.zqStatus != 2  ">
           您未中签
         </h1>
         <h1 class="c-title-text" v-else-if="!item.checkInTime">
           您已签到
+        </h1>
+        <h1 class="c-title-text" v-else>
+          签到成功
         </h1>
       </div>
       <div class="weui-cells__title c-text-center">
@@ -24,11 +27,11 @@
           </div>
         </h1>
       </div>
-      <!-- <div class="weui-cells__title">
+      <!--<div class="weui-cells__title">
         <h1 class="am-article-title">
           您已签到
         </h1>
-      </div> -->
+      </div>-->
       <div class="weui-form-preview">
         <div class="weui-form-preview__hd">
           <div v-if="item.checkInScore > 0" class="weui-form-preview__item">
