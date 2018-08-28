5ba4dd100dfc186bfd0d8e64dfab7960e6fbd96f
yangweiji <weiji.yang@9kylin.cn>
2018-08-28 19:26:54 +0800

    修改黑名单描述提示

--- src/main/resources/static/js/sec/community/thirduser/users.js
+++ src/main/resources/static/js/sec/community/thirduser/users.js
@@ -170,13 +170,11 @@ $(function () {
                         if(row.is_black==true) {
                             return '<button id="btnEdit" class="am-btn am-btn-sm am-btn-secondary" type="button" title="编辑用户"><i class="am-icon-edit"></i></button>'
                                 + '<button id="btnDelete" style="width: 42px" class="am-btn am-btn-sm am-btn-danger" type="button" title="删除用户"><i class="am-icon-trash-o"></i></button>'
-                               /* + '<button id="removeBlack" style="width: 42px;display: none" class="am-btn am-btn-sm am-btn-success" type="button" title="移除黑名单"><i class="fas fa-unlock-alt"></i></button>'*/
-                                + '<button id="addBlack" style="width: 42px" class="am-btn am-btn-sm am-btn-warning" type="button" title="移除黑名单"><i class="fas fa-lock"></i></button>'
+                                + '<button id="removeBlack" style="width: 42px" class="am-btn am-btn-sm am-btn-success" type="button" title="加入黑名单"><i class="fas fa-unlock-alt"></i></button>'
                         }else{
                             return '<button id="btnEdit" class="am-btn am-btn-sm am-btn-secondary" type="button" title="编辑用户"><i class="am-icon-edit"></i></button>'
                                 + '<button id="btnDelete" style="width: 42px" class="am-btn am-btn-sm am-btn-danger" type="button" title="删除用户"><i class="am-icon-trash-o"></i></button>'
-                                + '<button id="removeBlack" style="width: 42px" class="am-btn am-btn-sm am-btn-success" type="button" title="加入黑名单"><i class="fas fa-unlock-alt"></i></button>'
-                                /*+ '<button id="addBlack" style="width: 42px;display: none" class="am-btn am-btn-sm am-btn-warning" type="button" title="加入黑名单"><span class="fas fa-lock"></span></button>'*/
+                                + '<button id="addBlack" style="width: 42px" class="am-btn am-btn-sm am-btn-warning" type="button" title="移除黑名单"><i class="fas fa-lock"></i></button>'
                         }
                     }
                 }
@@ -256,7 +254,7 @@ $(function () {
             $.ajax({
                 type: 'post',
                 dataType: 'json',
-                url: '/sec/community/thirduser/removeBlack',
+                url: '/sec/community/thirduser/addBlack',
                 data: {
                     userId: data.id
                 },
@@ -278,7 +276,7 @@ $(function () {
             $.ajax({
                 type: 'post',
                 dataType: 'json',
-                url: '/sec/community/thirduser/addBlack',
+                url: '/sec/community/thirduser/removeBlack',
                 data: {
                     userId: data.id
                 },
