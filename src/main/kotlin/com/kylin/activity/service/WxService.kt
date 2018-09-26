aef01362c62193c07d8684967a2042a5bc141ef3
cf_hc1980 <cf_hc1980@aliyun.com>
2018-09-26 11:16:47 +0800

    1、更新weixin-java-pay:3.2.0 2、退款优化：未结算资金不足时，采用可用余额申请退款

--- src/main/kotlin/com/kylin/activity/service/WxService.kt
+++ src/main/kotlin/com/kylin/activity/service/WxService.kt
@@ -5,6 +5,7 @@ import com.github.binarywang.wxpay.bean.request.WxPayRefundRequest
 import com.github.binarywang.wxpay.bean.result.WxPayRefundResult
 import com.github.binarywang.wxpay.exception.WxPayException
 import com.github.binarywang.wxpay.service.WxPayService
+import com.kylin.activity.util.LogUtil
 import me.chanjar.weixin.mp.api.WxMpService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -22,9 +23,35 @@ class WxService {
     @Autowired
     val mpService: WxMpService? = null
 
+    /**
+     * 申请退款处理
+     */
     @Throws(WxPayException::class)
     fun refund(@RequestBody request: WxPayRefundRequest): WxPayRefundResult {
-        return payService!!.refund(request)
+        var result = WxPayRefundResult()
+        try {
+            result = payService!!.refund(request)
+            if (result.resultCode == "SUCCESS") {
+                LogUtil.printLog("REFUND_SOURCE_UNSETTLED_FUNDS, refund ok, outTradeNo: ${request.outTradeNo}, outRefundNo: ${request.outRefundNo}")
+            }
+        }
+        catch (e: WxPayException) {
+            LogUtil.printLog("err_code: ${e.errCode}, err_code_des: ${e.errCodeDes}, outTradeNo: ${request.outTradeNo}, outRefundNo: ${request.outRefundNo}")
+            if (e.resultCode == "FAIL" && e.errCode == "NOTENOUGH") {
+                //尝试使用可用余额退款
+                request.refundAccount = "REFUND_SOURCE_RECHARGE_FUNDS"
+                result = payService!!.refund(request)
+                //SUCCESS/FAIL
+                //SUCCESS退款申请接收成功，结果通过退款查询接口查询
+                //FAIL 提交业务失败
+                if (result.resultCode == "SUCCESS") {
+                    LogUtil.printLog("REFUND_SOURCE_RECHARGE_FUNDS, refund ok, outTradeNo: ${request.outTradeNo}, outRefundNo: ${request.outRefundNo}")
+                }
+            }
+        }
+        finally {
+        }
+        return result
     }
 
 
